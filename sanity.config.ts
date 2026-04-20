398a5d5c632a207c586f48561e61f6e3b12a912c
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-04-20 12:44:59 +0300

    updated data structure

--- sanity.config.ts
+++ sanity.config.ts
@@ -19,6 +19,85 @@ export default defineConfig({
           .title('Контент')
           .items([
             S.listItem()
+              .id('homePageContent')
+              .title('Головна сторінка')
+              .child(
+                S.list()
+                  .title('Головна сторінка')
+                  .items([
+                    S.listItem()
+                      .id('results')
+                      .title('Слайдер Results для головної сторінки')
+                      .child(
+                        S.editor()
+                          .id('results')
+                          .title('Слайдер Results для головної сторінки')
+                          .schemaType('results')
+                          .documentId('results'),
+                      ),
+                    S.listItem()
+                      .title('Команда')
+                      .schemaType('teamMember')
+                      .child(
+                        S.documentList()
+                          .title('Команда')
+                          .filter('_type == "teamMember"')
+                          .defaultOrdering([
+                            {field: 'order', direction: 'asc'},
+                            {field: 'name', direction: 'asc'},
+                          ]),
+                      ),
+                    S.listItem()
+                      .id('workingHours')
+                      .title('Години роботи')
+                      .child(
+                        S.editor()
+                          .id('workingHours')
+                          .title('Години роботи')
+                          .schemaType('workingHours')
+                          .documentId('workingHours'),
+                      ),
+                    S.listItem()
+                      .id('homeFaq')
+                      .title('FAQ для головної сторінки')
+                      .child(
+                        S.editor()
+                          .id('homeFaq')
+                          .title('FAQ для головної сторінки')
+                          .schemaType('homeFaq')
+                          .documentId('homeFaq'),
+                      ),
+                  ]),
+              ),
+            S.listItem()
+              .id('servicesPageContent')
+              .title('Сторінка Послуг')
+              .child(
+                S.list()
+                  .title('Сторінка Послуг')
+                  .items([
+                    S.listItem()
+                      .title('Послуги')
+                      .schemaType('service')
+                      .child(
+                        S.documentList()
+                          .title('Послуги')
+                          .filter('_type == "service"')
+                          .defaultOrdering([{field: 'title', direction: 'asc'}]),
+                      ),
+                    S.listItem()
+                      .id('servicesFaq')
+                      .title('FAQ для сторінки послуг')
+                      .child(
+                        S.editor()
+                          .id('servicesFaq')
+                          .title('FAQ для сторінки послуг')
+                          .schemaType('servicesFaq')
+                          .documentId('servicesFaq'),
+                      ),
+                  ]),
+              ),
+            S.listItem()
               .title('Статті блогу')
               .schemaType('blogPost')
               .child(
@@ -26,67 +105,6 @@ export default defineConfig({
                   .title('Статті блогу')
                   .filter('_type == "blogPost"')
                   .defaultOrdering([{field: '_createdAt', direction: 'desc'}]),
-              ),
-            S.listItem()
-              .title('Послуги')
-              .schemaType('service')
-              .child(
-                S.documentList()
-                  .title('Послуги')
-                  .filter('_type == "service"')
-                  .defaultOrdering([{field: 'title', direction: 'asc'}]),
-              ),
-            S.listItem()
-              .title('Команда')
-              .schemaType('teamMember')
-              .child(
-                S.documentList()
-                  .title('Команда')
-                  .filter('_type == "teamMember"')
-                  .defaultOrdering([
-                    {field: 'order', direction: 'asc'},
-                    {field: 'name', direction: 'asc'},
-                  ]),
-              ),
-            S.listItem()
-              .id('homeFaq')
-              .title('FAQ для головної сторінки')
-              .child(
-                S.editor()
-                  .id('homeFaq')
-                  .title('FAQ для головної сторінки')
-                  .schemaType('homeFaq')
-                  .documentId('homeFaq'),
-              ),
-            S.listItem()
-              .id('servicesFaq')
-              .title('FAQ для сторінки послуг')
-              .child(
-                S.editor()
-                  .id('servicesFaq')
-                  .title('FAQ для сторінки послуг')
-                  .schemaType('servicesFaq')
-                  .documentId('servicesFaq'),
-              ),
-            S.listItem()
-              .id('workingHours')
-              .title('Години роботи')
-              .child(
-                S.editor()
-                  .id('workingHours')
-                  .title('Години роботи')
-                  .schemaType('workingHours')
-                  .documentId('workingHours'),
-              ),
-            S.listItem()
-              .id('results')
-              .title('Слайдер Results для головної сторінки')
-              .child(
-                S.editor()
-                  .id('results')
-                  .title('Слайдер Results для головної сторінки')
-                  .schemaType('results')
-                  .documentId('results'),
               ),
           ]),
     }),
